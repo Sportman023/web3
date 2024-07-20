94db8b77f05b648aef01964a57c3a76d23dc034d
Vadim Ortman <v_ortman@twistellar.com>
2024-07-20 23:52:02 +0300

    Refactor uniswap

--- src/evm/uniswap.ts
+++ src/evm/uniswap.ts
@@ -4,11 +4,11 @@ import { ethers } from 'ethers';
 const INFURA_URL_MAINNET = process.env.INFURA_URL_MAINNET as string;
 
 export class UniswapService {
-    private ethUsdtConfig: any;
+    private pairConfig: any;
     private contract: any;
 
-    constructor(ethUsdtConfig: any) {
-        this.ethUsdtConfig = ethUsdtConfig;
+    constructor(pairConfig: any) {
+        this.pairConfig = pairConfig;
         this.setupContract();
     }
 
@@ -19,8 +19,7 @@ export class UniswapService {
         const slot0 = await this.contract.slot0();
         const sqrtPriceX96 = Number(slot0.sqrtPriceX96);
 
-        const token0Decimals = this.ethUsdtConfig.get('token0Decimals');
-        const token1Decimals = this.ethUsdtConfig.get('token1Decimals');
+        const { token0Decimals, token1Decimals } = this.pairConfig;
 
         const buyOneOfToken0 =
             (Number(sqrtPriceX96) / 2 ** 96) ** 2 /
@@ -36,29 +35,9 @@ export class UniswapService {
     private async setupContract(): Promise<void> {
         const provider = new ethers.JsonRpcProvider(INFURA_URL_MAINNET);
         this.contract = new ethers.Contract(
-            this.ethUsdtConfig.get('contractAddress'),
-            this.ethUsdtConfig.get('abi'),
+            this.pairConfig.get('contractAddress'),
+            this.pairConfig.get('abi'),
             provider
         );
-    }
-
-    public startTrackPairs() {
-        setInterval(async () => {
-            const { buyOneOfToken0, buyOneOfToken1 } = await this.getPrice();
-            const token0Symbol = this.ethUsdtConfig.get(
-                'token0Symbol'
-            ) as string;
-            const token1Symbol = this.ethUsdtConfig.get(
-                'token1Symbol'
-            ) as string;
-
-            let result: any = {
-                [token0Symbol]: buyOneOfToken0,
-                [token1Symbol]: buyOneOfToken1,
-            };
-
-            console.log({ ...result });
-            console.log('------------------------\n');
-        }, 5000);
     }
 }
